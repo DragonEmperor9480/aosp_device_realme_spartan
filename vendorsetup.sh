873d6d4c1231cddadfc3909c484c05bc8122dcfc
Amrutesh Naregal <107930548+DragonEmperor9480@users.noreply.github.com>
2026-06-28 18:11:32 +0530

    spartan: initial vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+#common tree
+git clone https://github.com/DragonEmperor9480/aosp_device_realme_sm8250-common.git -b 17 device/realme/sm8250-common
+
+#kernel
+git clone https://github.com/provasish/android_kernel_qcom_sm8250.git kernel/oneplus/sm8250
+
+#vendor
+git clone https://github.com/provasish/proprietary_vendor_realme_sm8250-common.git vendor/realme/sm8250-common
+git clone https://github.com/provasish/proprietary_vendor_realme_spartan.git vendor/realme/spartan
